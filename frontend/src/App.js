f94fca066df83c72e5ca8f750c663a4dc0e399e5
SiddhantVgaikwad <128611331+SiddhantVgaikwad@users.noreply.github.com>
2024-05-26 12:53:05 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Transactions from './components/Transactions/Transactions';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Transactions/>
+    </div>
+  );
+}
+
+export default App;
